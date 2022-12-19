7c575cdd40dadb8522cf243fedfbb1807cfd6aa7
Ellielton <ellieltongama@gmail.com>
2022-12-18 22:09:40 -0300

    Versão atualizado do pc

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var multiplicador = document.querySelector(".multiplicador");
 var multiplicando = document.querySelector(".multiplicando");
 
 var resposta = document.querySelector(".resposta");
+resposta.focus();
 
 var conferi = document.querySelector(".conferi");
 
@@ -10,34 +11,41 @@ const start = document.querySelector(".start");
 
 var mensagem = document.querySelector(".mensagem");
 
+var ganhouPontos = document.querySelector(".ganhouPontos");
+
+var perdeuPontos = document.querySelector(".perdeuPontos");
+
 // funçao que inicia o jogo ao click do botão start, faz gerar dois digitos aleatórios e executa multiplicação entre ambos.
 
-  function iniciarJogo() {
-    var operador1 = 9 /*Math.round(Math.random() * 10);*/
-    multiplicador.value = operador1
-    
-    var operador2 = Math.round(Math.random() * 10);
-    multiplicando.value = operador2
-    
+function iniciarJogo() {
+  var operador1 = 5 //Math.round(Math.random() * 10);
+  multiplicador.value = operador1
+  
+  var operador2 = Math.round(Math.random() * 10);
+  multiplicando.value = operador2
+  
+  mensagem.value = "";
+  resposta.value = "";
+  
+}
 
+start.onclick = iniciarJogo;
+  
+var maisPontos = 1;
+var perdeu = ganhouPontos.value;
+
+function verifica(){
+  var produto = multiplicador.value * multiplicando.value;
+
+  if(produto == resposta.value ){
+    
+    mensagem.value = "VOCÊ ACERTOU!";
+    ganhouPontos.value = maisPontos++
+    
+  } else {
+    
+    mensagem.value = "VOCÊ ERROU!!!";
+    perdeuPontos.value = perdeu--
   }
-  start.onclick = iniciarJogo;
-  
-  
-  function verifica(){
-    var produto = multiplicador.value * multiplicando.value;
-
-    
-    if(produto == resposta.value ){
-      
-     mensagem.value = "VOCÊ ACERTOU!"
-      
-    } else {
-      
-      mensagem.value = "VOCÊ ERROU!!!";
-    }
-  }
-    mensagem.value = ("");
-  conferi.onclick = verifica;
-  
-
+}
+conferi.onclick = verifica;
